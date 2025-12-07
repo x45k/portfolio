01634da09257d77808d94820127144a8551c64d4
x45k <157172151+x45k@users.noreply.github.com>
2025-12-06 18:18:55 -0600

    pizza things

---
+++ pizza/script.js
@@ -0,0 +1,15 @@
+const menuToggle = document.getElementById('menuToggle');
+const sidebar = document.getElementById('sidebar');
+        
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    sidebar.classList.toggle('active');
+});
+        
+menuToggle.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+});
+        
+menuToggle.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+});
